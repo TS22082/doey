668966c7faeca2754c5957f065cabfe6c16cc5d1
GeekCentric <ts22082@gmail.com>
2019-06-28 20:13:52 -0700

    add a route to return all todos

--- server.js
+++ server.js
@@ -8,16 +8,24 @@ app.use(express.json())
 
 var todos = []
 
+//serve html
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/client/index.html'))
 })
 
+// to show all todos
+app.get('/api/todos', function(req, res) {
+  return res.json(todos)
+})
+
+//to add a todo
 app.post('/api/todo', function(req, res) {
   var newTodo = req.body
   todos.push(newTodo)
   res.json(todos)
 })
 
+//start server
 app.listen(PORT, function() {
   console.log('App listening on PORT ' + PORT)
 })
